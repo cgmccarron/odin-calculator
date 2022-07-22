b067c723406de552beccc53143c77d43c5e9c330
cgmccarron <mccarron.christian13@gmail.com>
2022-07-22 00:16:45 -0500

    changed some styling for darkthem icon

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const icon = document.getElementById("icon");
 const content = document.querySelector(".content");
+
 icon.addEventListener("click", () => {
+  //This is the toggle between light and dark themes
   content.classList.toggle("darkTheme");
+  //This switches the icon of sun to moon and visa versa
   if (content.classList.contains("darkTheme")) {
     icon.src = "./images/moon-icon.png";
   } else {
